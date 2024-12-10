e4d869775b663523b80a33a689efdfa24933d706
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2024-12-10 23:31:26 +0100

    Add files via upload

---
+++ exo12.py
@@ -0,0 +1,11 @@
+hash_width = int(input ("please type in the width of the hash :"))
+hash_height = int(input ("please type in the height of the hash :"))
+
+
+i=1
+hash=""
+while  i<=hash_width :
+   hash=hash+"#"
+   i=i+1
+print(hash)
+

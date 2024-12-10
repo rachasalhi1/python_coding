a634f7a17fbf4aba268a225d3402f9d7c239c11b
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2024-12-10 23:29:51 +0100

    Add files via upload

---
+++ exo8.py
@@ -0,0 +1,7 @@
+number =int(input("Number :"))
+if number%3== 0 and number %5 !=0:
+    print("Fizz")
+if number %5 ==0 and number%3!= 0:
+    print("Buzz")
+if number%3== 0 and number %5 ==0 :
+     print("FizzBuzz")

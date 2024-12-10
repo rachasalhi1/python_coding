7ef13dd1c0aff1b2f6fdfeb96a816ae361861253
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2024-12-10 23:28:11 +0100

    Add files via upload

---
+++ exo4.py
@@ -0,0 +1,8 @@
+age_1=int(input("please type in the age of the first person :"))
+age_2=int(input("please type in the age of the second person :"))
+if age_1>age_2:
+   print(f"the older age is {age_1}") 
+elif age_1<age_2:
+   print(f"the older age is {age_1}") 
+else:
+   print("they are same age")

97d833c18584c93ab5ee2095a68e5a0b6ce8fd6d
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2024-12-11 22:56:42 +0100

    Add files via upload

---
+++ exo15.py
@@ -0,0 +1,23 @@
+string=input("please enter a string :")
+a=False
+e=False
+o=False
+for x in string:
+  if  x=='a':
+   a=True
+  if x=='e':
+    e=True
+  if x=='o':
+   o=True
+if a==True:
+ print("a found")
+else :
+ print("a not found")
+if e==True:
+ print("e found")
+else:
+ print("e not found")
+if  o==True:
+ print("o found")
+else:
+ print("e not found")  
